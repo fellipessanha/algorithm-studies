8b49bfe99d9f6b266ba9f9b67fd11901492ad88e
fellipessanha <fellipe900@gmail.com>
2025-09-05 20:56:16 -0300

    daily exercise

---
+++ leetcode/go/search_insert_position.go
@@ -0,0 +1,36 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+	if target < nums[0] {
+		return 0
+	}
+	if target > nums[len(nums)-1] {
+		return len(nums)
+	}
+	var l, r = 0, len(nums)
+	for l <= r {
+		m := l + (r-l)/2
+		if m >= len(nums) || m <= 0 || nums[m] == target || (nums[m-1] < target && nums[m] > target) {
+			return m
+		}
+		if nums[m] < target {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+	return -1
+}
+
+func main() {
+	candidate := []int{1, 3, 5, 6}
+	for _, v := range []int{0, 5, 2, 8} {
+		output := searchInsert(candidate, v)
+		println(output)
+	}
+
+	println(searchInsert([]int{1}, 2))
+	println(searchInsert([]int{1}, 1))
+	println(searchInsert([]int{1}, 0))
+
+}
